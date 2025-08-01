ea01833770d6bc11b230842d048b8021c21fc75f
Patrick Hughes <phughes@tutanota.de>
2025-07-31 23:44:11 -0500

    moved to root

---
+++ include/system.h
@@ -0,0 +1,18 @@
+#ifndef __SYSTEM_H
+#define __SYSTEM_H
+
+/* MAIN.C */
+extern unsigned char *memcpy(unsigned char *dest, const unsigned char *src, int count);
+extern unsigned char *memset(unsigned char *dest, unsigned char val, int count);
+extern unsigned short *memsetw(unsigned short *dest, unsigned short val, int count);
+extern int strlen(const char *str);
+extern unsigned char inportb(unsigned short _port);
+extern void outportb(unsigned short _port, unsigned char _data);
+
+/* KERNEL.C */
+extern void cls();
+extern void terminal_putchar(char c);
+extern void terminal_writestring(const char *data);
+extern void terminal_initialize();
+
+#endif

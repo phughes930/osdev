ebdbb8a64522d313c02bc5ade4817f26ca711a8a
Patrick Hughes <phughes@tutanota.de>
2025-08-12 23:14:54 -0500

    set up the GDT

--- src/mb2_boot.s
+++ src/mb2_boot.s
@@ -34,6 +34,16 @@ stack_bottom:
 .skip 16384         # 16Kb
 stack_top:
 
+.align 2
+gdtr:
+    .word   # should be 0x20
+    .long   # should be an address
+
+gdt_bottom:
+.skip 32            # 8 bytes x 2 levels x 2 types
+gdt_top:
+
+
 saved_eax: .skip 4
 saved_ebx: .skip 4
 
@@ -76,12 +86,93 @@ start:
     movl    saved_ebx, %eax
     addl    $12, %edi
     call    print_hex
+    subl    $12, %edi
 
-    call    term_main
+    #   GDT debug
+    call    set_gdtr
+
+    movl    $gdt_message, %esi
+    addl    $160, %edi
+    call    print_string
+    
+    #       GDT size
+    xorl    %eax, %eax
+    movw    gdtr, %ax
+    addl    $12, %edi
+    call    print_hex
+    subl    $12, %edi
+
+    #       GDT loc
+    movl    gdtr+2, %eax
+    addl    $34, %edi
+    call    print_hex
+    subl    $34, %edi
+
+    lgdt    gdtr
+
+    #   flags & limit high 4 bits in %al
+    #   access byte in %dl
+    #   setting up the gdt
+    movl    $0, %ecx
+    call    set_gdt_descr
+
+    #   debug the null descriptor
+    movl    $gdt_desc_message, %esi
+    addl    $160, %edi
+    call    print_string
+
+    movl    gdt_bottom, %eax
+    addl    $18, %edi
+    call    print_hex
+    addl    $22, %edi
+    movl    gdt_bottom+4, %eax
+    call    print_hex
+    subl    $40, %edi
+    
+    #   set up a kernel code segment
+    incl    %ecx
+    movb    $0xCF, %al
+    movb    $0x9A, %dl
+    call    set_gdt_descr
+
+    #   debug the kernel code segment
+    movl    $gdt_desc_message, %esi
+    addl    $160, %edi
+    call    print_string
+
+    movl    gdt_bottom+8, %eax
+    addl    $18, %edi
+    call    print_hex
+    addl    $22, %edi
+    movl    gdt_bottom+12, %eax
+    call    print_hex
+    subl    $40, %edi
+    
+    #   set up a kernel data segment
+    movl    $2, %ecx
+    movb    $0xCF, %al
+    movb    $0x92, %dl
+    call    set_gdt_descr
+
+    #   debug the kernel data segment
+    movl    $gdt_desc_message, %esi
+    addl    $160, %edi
+    call    print_string
+
+    movl    gdt_bottom+16, %eax
+    addl    $18, %edi
+    call    print_hex
+    addl    $22, %edi
+    movl    gdt_bottom+20, %eax
+    call    print_hex
+    
+    // call    term_main
 
 halt_loop:
     hlt
     jmp     halt_loop
+
+isrs:
 
 
 # cls: clear screen
@@ -164,6 +255,69 @@ hex_digit:
     popa
     ret
 
+set_gdtr:
+    # get size
+    movl    $gdt_top, %eax
+    movl    $gdt_bottom, %edx
+    subl    %edx, %eax
+    movw    %ax, gdtr
+
+    # get address
+    movl    $gdt_bottom, %eax
+    movl    %eax, gdtr+2
+    ret
+
+#   flags & limit high 4 bits in %al
+#   access byte in %dl
+#   descriptor number in %ecx
+#   limit = 0x0F FF FF
+#   base = 0x00 00 00 00;
+set_gdt_descr:
+    pushl   %edi
+    pushl   %ebx
+    # get memory address for current segment
+    movl    $gdt_bottom, %edi
+
+    # determine if we are making the null descriptor
+    testl   %ecx, %ecx
+    jz      set_null_descr
+
+    # otw, set the pointer
+    imull   $8, %ecx
+    addl    %ecx, %edi
+
+    # set the low four bytes of the descriptor
+    # this will always be the same
+    movl    $0x0000FFFF, %ebx
+    movl    %ebx, (%edi)
+    addl    $4, %edi
+    
+    # set up for the high four bytes now
+    # stage it in %ebx
+    xorl    %ebx, %ebx
+    # flags and high limit
+    orb     %al, %bh
+    # access byte
+    orb     %dl, %bl
+    sall    $8, %ebx
+    orb     $0xFF, %bl
+    # mov to gdt seg
+    movl    %ebx, (%edi)
+    
+    jmp     finish_gdt_seg
+
+set_null_descr:
+    movl    $0, %eax
+    movl    %eax, (%edi)
+    
+    addl    $4, %edi
+    movl    %eax, (%edi)
+
+finish_gdt_seg:
+    popl    %ebx
+    popl    %edi
+    ret
+
 .section .rodata
 welcome_message:
     .asciz "Welcome to PatOS"
@@ -171,3 +325,7 @@ eax_message:
     .asciz "EAX: "
 ebx_message:
     .asciz "EBX: "
+gdt_message:
+    .asciz "GDT: "
+gdt_desc_message:
+    .asciz "GDT Seg: "
